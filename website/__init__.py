8830357032e2821d77a3c97b5d5bc57bc791851a
Aroosh Moulik <arooshmoulik@Arooshs-MacBook-Air.local>
2021-11-26 10:04:12 -0500

    Changed secret key and edited password display

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app():
     app=Flask(__name__)
-    app.config['SECRET_KEY'] = 'ARRROOOO'
+    app.config['SECRET_KEY'] = 'secret_aroosh_key'
     from .views import views
     from .auth import auth
 
